260f501a6114fa343e49286b45917d3085a9e1df
Saurabh Kumar~ <hmsaurabhkr@gmail.com~>
2022-09-25 23:15:56 +0530

    fix bold and underline bug

--- index.js
+++ index.js
@@ -4,23 +4,35 @@ const underlineButton = document.querySelector("#underline");
 
 text.contentEditable = "true";
 
-// Bold the selected text
-function makeTextBold(e) {
+function bold(e) {
   e.preventDefault();
-  const selecText = window.getSelection().getRangeAt(0);
-  const boldText = "<b>" + selecText + "</b>";
-  text.innerHTML = text.innerHTML.replace(selecText, boldText);
-  console.log(text.innerHTML);
+  {
+    document.execCommand("bold");
+  }
 }
+
+function underline(e) {
+  e.preventDefault();
+  document.execCommand("underLine");
+}
+
+boldButton.addEventListener("click", bold);
+underlineButton.addEventListener("click", underline);
+
+// Bold the selected text
+// function makeTextBold(e) {
+//   e.preventDefault();
+//   const selecText = window.getSelection().getRangeAt(0);
+//   const boldText = "<b>" + selecText + "</b>";
+//   text.innerHTML = text.innerHTML.replace(selecText, boldText);
+//   console.log(text.innerHTML);
+// }
 
 // Underline the selected text
-function makeTextUnderline(e) {
-  e.preventDefault();
-  const selecText = window.getSelection().getRangeAt(0);
-  const underlineText = `<span style="text-decoration: underline">${selecText}</span>`;
-  text.innerHTML = text.innerHTML.replace(selecText, underlineText);
-  console.log(text.innerHTML);
-}
-
-boldButton.addEventListener("click", makeTextBold);
-underlineButton.addEventListener("click", makeTextUnderline);
+// function makeTextUnderline(e) {
+//   e.preventDefault();
+//   const selecText = window.getSelection().getRangeAt(0);
+//   const underlineText = `<span style="text-decoration: underline">${selecText}</span>`;
+//   text.innerHTML = text.innerHTML.replace(selecText, underlineText);
+//   console.log(text.innerHTML);
+// }
